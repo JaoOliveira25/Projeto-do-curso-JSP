05ea24df348dfa018ece78d51ec2b16c7a67a778
João Vitor <--get>
2025-02-18 07:47:36 -0300

    Criando classe DAOLogin e método de autenticação no com banco de dados

---
+++ projeto-curso-jsp/src/main/java/dao/DAOLoginRepository.java
@@ -0,0 +1,35 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import connection.SingleConnectionBanco;
+import model.ModelLogin;
+
+public class DAOLoginRepository {
+	//precisamos de um objeto de conexão com o BD 
+	private Connection connection;
+
+	public DAOLoginRepository() {
+		this.connection = SingleConnectionBanco.getConnection();
+	}
+	
+	//Agora validamos nosso login passamos o objeto Model como parametro
+	public boolean validarAutenticacao(ModelLogin modelLogin) throws Exception{
+		//crie o sql de consulta "select" com o uso de parametros para evitar sql injection
+		String sql = "select * from model_login where upper(login) = login=? and upper(password) = password=? ";
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, modelLogin.getLogin());
+		statement.setString(2, modelLogin.getSenha());
+		
+		ResultSet resultSet = statement.executeQuery();
+		
+		if(resultSet.next()) {
+			return true;//autenticado
+		}
+		return false;// não autenticado 
+		
+	}
+	
+}
--- projeto-curso-jsp/src/main/java/servlets/ServletLogin.java
+++ projeto-curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -1,5 +1,8 @@
 package servlets;
 
+import java.io.IOException;
+
+import dao.DAOLoginRepository;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -7,61 +10,62 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
-
-import java.io.IOException;
 //nota sempre que foir criar uma servlet apague o código xml gerado automaticamente para mapeamento 
 //toda servlet importa os pacotes de servlet do jakarta 
 //servlets são os controller de um projeto MVC ServletLoginControllerb
 
-@WebServlet("/ServletLogin") //mapeamento de url que vem da tela
+@WebServlet("/ServletLogin") // mapeamento de url que vem da tela
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  
-    public ServletLogin() {
-        super();
-        
-    }
+	private DAOLoginRepository daoLoginRepository = new DAOLoginRepository();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
+	public ServletLogin() {
+		super();
+
 	}
 
-	//recebe os dados enviados por formulário 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	// recebe os dados enviados por formulário
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
 		String url = request.getParameter("url");
-		
-		if(login!=null && !login.isEmpty() && senha!= null && !senha.isEmpty()) {
-			ModelLogin modelLogin = new ModelLogin();
-			modelLogin.setLogin(login);
-			modelLogin.setSenha(senha);
-			if(modelLogin.getLogin().equalsIgnoreCase("admin")
-					&& modelLogin.getSenha().equalsIgnoreCase("admin")) {
-				
-				request.getSession().setAttribute("usuario", modelLogin.getLogin());
-				
-				if(url == null|| url.equals("null")) {
-					url = "principal/principal.jsp";
+
+		try {
+			if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+				ModelLogin modelLogin = new ModelLogin();
+				modelLogin.setLogin(login);
+				modelLogin.setSenha(senha);
+
+				if (daoLoginRepository.validarAutenticacao(modelLogin)) {
+
+					request.getSession().setAttribute("usuario", modelLogin.getLogin());
+
+					if (url == null || url.equals("null")) {
+						url = "principal/principal.jsp";
+					}
+
+					RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+					redirecionar.forward(request, response);
+
+				} else {
+					RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+					request.setAttribute("msg", "Informe o login e a senha corretamente");
+					redirecionar.forward(request, response);
 				}
-				
-				RequestDispatcher redirecionar = request.getRequestDispatcher(url);
-				redirecionar.forward(request, response);
-				
-			}else {
-				RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+			} else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
 				request.setAttribute("msg", "Informe o login e a senha corretamente");
 				redirecionar.forward(request, response);
 			}
-		}else {
-			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
-			request.setAttribute("msg", "Informe o login e a senha corretamente");
-			redirecionar.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-		
 
 	}
 
