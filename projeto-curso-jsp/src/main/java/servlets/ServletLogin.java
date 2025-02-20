a389d34c9af759c6860c612d6c7e59afc3267b01
João Vitor <--get>
2025-02-20 11:29:39 -0300

    Criando o Logout e encerramento da seção do usuário no sistema

--- projeto-curso-jsp/src/main/java/servlets/ServletLogin.java
+++ projeto-curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -26,7 +26,16 @@ public class ServletLogin extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doPost(request,response);
+		String acao = request.getParameter("acao");
+		if(acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("logout")) {
+			request.getSession().invalidate();// invalida os atributos setados na session
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			redirecionar.forward(request, response);
+			
+		}else {
+			doPost(request,response);
+		}
+		
 	}
 
 	// recebe os dados enviados por formulário
